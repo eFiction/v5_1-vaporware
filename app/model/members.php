b885cb921ed58b7d74ed7e202ff0f67af72c654e
Rainer Volkrodt <rainer@volkrodt.de>
2021-01-31 17:47:12 +0100

    Member profile: fix counting stories that were not yet published

--- app/model/members.php
+++ app/model/members.php
@@ -21,10 +21,10 @@ class Members extends Base
 							LEFT JOIN `tbl_user_fields`F ON ( I.field = F.field_id )
 					WHERE U.uid = {$uid}
 					GROUP BY U.uid" )[0];
-			
+
 			$user['fields'] = $this->cleanResult($user['fields']);
 
-			$stats[] = "SET @stories  := (SELECT COUNT(DISTINCT S.sid) 
+			$stats[] = "SET @stories  := (SELECT COUNT(DISTINCT S.sid)
 											FROM `tbl_stories_authors`rSA
 												LEFT JOIN `tbl_stories`S ON ( S.sid = rSA.sid
 															AND S.validated >= 20
@@ -55,10 +55,10 @@ class Members extends Base
 		if ( "" == $library = \Cache::instance()->get('memberProfileCache_'.$uid.'_LibV_'.$visibility) )
 		{
 			$status = $visibility<2 ? "('F','P','A')" : "('P','A')";
-			$libSQL[] = "SET @series  := (SELECT COUNT(DISTINCT C.collid) 
+			$libSQL[] = "SET @series  := (SELECT COUNT(DISTINCT C.collid)
 											FROM `tbl_collections`C
 										WHERE C.uid = {$uid} AND C.ordered=1 AND C.status IN {$status});";
-			$libSQL[] = "SET @collections  := (SELECT COUNT(DISTINCT C.collid) 
+			$libSQL[] = "SET @collections  := (SELECT COUNT(DISTINCT C.collid)
 											FROM `tbl_collections`C
 										WHERE C.uid = {$uid} AND C.ordered=0 AND C.status IN {$status});";
 			$libSQL[] = "SET @favourites := (SELECT GROUP_CONCAT(V SEPARATOR '||') FROM (SELECT CONCAT(COUNT(DISTINCT F.fid),',',F.type) as V  FROM `tbl_user_favourites`F WHERE `uid` = {$uid} AND `bookmark` = 0 AND `visibility` >= {$visibility} GROUP BY `type`) AS T);";
@@ -86,22 +86,22 @@ class Members extends Base
 			}
 			\Cache::instance()->set('memberProfileCache_'.$uid.'_LibV_'.$visibility, $library, 300);
 		}
-		
+
 		// merge the library data into the user data
 		$user['extras'] = array_merge($user['extras'], $library);
 		// check if we are friends with the user
 		$user['friend'] = $this->exec("SELECT Fr.link_id FROM `tbl_user_friends`Fr WHERE Fr.user_id = {$_SESSION['userID']} AND Fr.friend_id = {$uid} AND active = 1;")[0]??FALSE;
 		$user['visibility'] = $visibility;
-		
+
 		return $user;
 	}
-	
+
 	public function memberStories(array $author, array $options): array
 	{
 		$data = $this->profileGetCount($author['uid']);
-			
+
 		$limit = $this->config['stories_per_page'];
-		
+
 		$pos = (int)\Base::instance()->get('paginate.page') - 1;
 
 		$replacements =
@@ -111,29 +111,29 @@ class Members extends Base
 			"JOIN" => "INNER JOIN `tbl_stories_authors`rSA ON ( rSA.sid=S.sid AND rSA.aid=:aid )"
 		];
 		$data['stories'] = $this->storyData($replacements, ["aid" => $author['uid']]);
-		
+
 		$this->paginate(
 			$this->exec("SELECT FOUND_ROWS() as found")[0]['found'],
 			"/members/{$author['username']}/stories",
 			$limit
 		);
-		
+
 		return $data;
 	}
-	
+
 	public function memberCollections(array $userData = [], string $selection)
 	{
 		$ordered = ($selection=="series");
 		// common SQL creation for member profile and story view
 		list ( $sql, $limit ) = $this->collectionsListBase($userData, $ordered);
 		$data = $this->exec($sql);
-				
+
 		$this->paginate(
 			$this->exec("SELECT FOUND_ROWS() as found")[0]['found'],
 			$ordered ? "/members/{$userData['username']}/series" : "/members/{$userData['username']}/collections",
 			$limit
 		);
-		
+
 		if ( sizeof($data)>0 )
 		{
 			foreach ( $data as &$dat)
@@ -157,9 +157,10 @@ class Members extends Base
 			"cat"	=>
 			[
 				"name" => "Categories",
-				"sql"	=> "SELECT SQL_CALC_FOUND_ROWS 
+				"sql"	=> "SELECT SQL_CALC_FOUND_ROWS
 							C.category as name, C.cid, count(C.cid) as counted
 								FROM `tbl_stories_authors`rSA
+									INNER JOIN `tbl_stories`S ON (rSA.sid = S.sid AND (S.completed >= 6 AND S.validated >= 30))
 									INNER JOIN `tbl_stories_categories`rSC ON (rSC.sid = rSA.sid AND (rSA.type='M' OR rSA.type='S') AND rSA.aid = :uid)
 								LEFT JOIN `tbl_categories`C ON ( rSC.cid = C.cid )
 							GROUP BY C.cid
@@ -168,9 +169,10 @@ class Members extends Base
 			"tag"	=>
 			[
 				"name" => "Categories",
-				"sql"	=> "SELECT SQL_CALC_FOUND_ROWS 
+				"sql"	=> "SELECT SQL_CALC_FOUND_ROWS
 							T.label, T.tid, count(T.tid) as counted
 								FROM `tbl_stories_authors`rSA
+									INNER JOIN `tbl_stories`S ON (rSA.sid = S.sid AND (S.completed >= 6 AND S.validated >= 30))
 									INNER JOIN `tbl_stories_tags`rST ON (rSA.sid = rST.sid AND rST.character=0 AND (rSA.type='M' OR rSA.type='S') AND rSA.aid = :uid)
 										INNER JOIN `tbl_tags`T ON ( rST.tid = T.tid AND T.tgid = 1 )
 							GROUP BY T.tid
@@ -179,16 +181,17 @@ class Members extends Base
 			"char"	=>
 			[
 				"name" => "Categories",
-				"sql"	=> "SELECT SQL_CALC_FOUND_ROWS 
+				"sql"	=> "SELECT SQL_CALC_FOUND_ROWS
 							Ch.charname, Ch.charid, count(Ch.charid) as counted
 								FROM `tbl_stories_authors`rSA
+									INNER JOIN `tbl_stories`S ON (rSA.sid = S.sid AND (S.completed >= 6 AND S.validated >= 30))
 									INNER JOIN `tbl_stories_tags`rSC ON (rSA.sid = rSC.sid AND rSC.character=1 AND (rSA.type='M' OR rSA.type='S') AND rSA.aid = :uid)
 										INNER JOIN `tbl_characters`Ch ON ( rSC.tid = Ch.charid )
 							GROUP BY Ch.charid
 							ORDER BY counted DESC, Ch.charname ASC"
 			],
 		];
-		
+
 		// run through the jobs defined above
 		foreach ( $jobs as $key => $fields )
 		{
@@ -216,7 +219,7 @@ class Members extends Base
 		$data = $this->loadFavourites($author, $options, $page, TRUE);
 		return $data;
 	}
-	
+
 	public function loadFavourites(array $author, array $options, int $page, bool $bookmarks=FALSE): array
 	{
 		// visibility scope required
@@ -260,22 +263,22 @@ class Members extends Base
 				"join"		=> "Rec.recid = Fav.item AND Fav.type='RC'"
 			]
 		];
-		
+
 		if( empty($select[$options[0]]) )
 			return [];
-		
+
 		$limit = 10;
 		$pos = $page - 1;
 
-		$sql = "SELECT SQL_CALC_FOUND_ROWS {$select[$options[0]]['fields']}, 
+		$sql = "SELECT SQL_CALC_FOUND_ROWS {$select[$options[0]]['fields']},
 						Fav.comments, Fav.visibility, Fav.notify, Fav.fid, Fav.bookmark
-						FROM {$select[$options[0]]['from']} 
+						FROM {$select[$options[0]]['from']}
 						INNER JOIN `tbl_user_favourites`Fav ON
 					( {$select[$options[0]]['join']} AND Fav.uid = {$author['uid']} AND Fav.visibility >= '{$visibility}' AND Fav.bookmark = ".(int)$bookmarks." )
-					LIMIT ".(max(0,$pos*$limit)).",".$limit;		
+					LIMIT ".(max(0,$pos*$limit)).",".$limit;
 
 		$data = $this->exec( $sql );
-		
+
 		$this->paginate(
 			$this->exec("SELECT FOUND_ROWS() as found")[0]['found'],
 			"/members/{$author['username']}/".(($bookmarks)?"bookmarks":"favourites")."/{$options[0]}",
@@ -284,5 +287,5 @@ class Members extends Base
 
 		return $data;
 	}
-	
+
 }
